58d8740ce9cb2e2270ec3efdef50a5ee7e42f097
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-04 14:36:23 +0100

    Removing given number from linked list

---
+++ python/easy/remove_linked_list_elements.py
@@ -0,0 +1,49 @@
+""" 
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Example 1:
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        temp = ListNode(val=-1, next=head)
+        prev = temp
+        curr = head
+
+        while curr:
+            if curr.val != val:
+                prev = curr
+                curr = curr.next
+            else:
+                while curr and curr.val == val:
+                    curr = curr.next
+                if not curr:
+                    prev.next = None
+                    return temp.next
+                else:
+                    prev.next = curr
+
+        return temp.next
