8c95f9bcbb0fd76b55112a2e20bcab9f16707dd4
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-07 13:27:59 +0100

    finding middle of list

---
+++ python/easy/middle_node.py
@@ -0,0 +1,43 @@
+""" 
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+"""
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None:
+            return head
+
+        curr = head
+        counter = 0
+        while curr != None:
+            counter += 1
+            curr = curr.next
+
+        for _ in range(int(counter/2)):
+            head = head.next
+
+        return head
