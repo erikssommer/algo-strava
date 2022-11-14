55fa2df03994778bc89dddcaef3e523b7fd0c792
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-14 13:24:28 +0100

    Search for value in binary tree

---
+++ python/easy/search_binary_tree.py
@@ -0,0 +1,41 @@
+"""
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+Example 1:
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Example 2:
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+"""
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val == val:
+                    return node
+
+                stack.append(node.left)
+                stack.append(node.right)
+
+        return None
