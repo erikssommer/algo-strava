9f6c9ff37b0934f3e0ba9507214505166d4e9eca
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-14 13:14:58 +0100

    Traversing in pre order binary tree

---
+++ python/easy/preorder_binary_tree.py
@@ -0,0 +1,47 @@
+"""
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+
+Example 3:
+
+Input: root = [1]
+Output: [1]
+"""
+
+# Definition for a binary tree node.
+from ast import List
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        preorder = []
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                preorder.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+
+        return preorder
