223e4eb4b9446c511bdc6dcf9f5dd595c042dbac
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-14 13:51:30 +0100

    Formatting

--- python/easy/postorder_binary_tree.py
+++ python/easy/postorder_binary_tree.py
@@ -23,22 +23,23 @@ from typing import Optional
 
 
 class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
-         
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         postorder = []
-        
+
         stack = [root]
-        
+
         while stack:
             node = stack.pop()
             if node:
                 postorder.append(node.val)
                 stack.append(node.left)
                 stack.append(node.right)
-            
+
         return postorder[::-1]
