ba8a0a2cf86b3c8a57049e131d38c0f949aa7dba
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-15 20:56:10 +0100

    Formatting docstring

--- java/easy/BinaryTreeInorder.java
+++ java/easy/BinaryTreeInorder.java
@@ -1,26 +1,27 @@
 package java.easy;
 
-/**
-Given the root of a binary tree, return the inorder traversal of its nodes' values.
-
-Example 1:
-
-Input: root = [1,null,2,3]
-Output: [1,3,2]
-
-Example 2:
-
-Input: root = []
-Output: []
-
-Example 3:
-
-Input: root = [1]
-Output: [1]
- */
-
 import java.util.LinkedList;
 import java.util.List;
+
+/**
+ * Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ * 
+ * Example 1:
+ * 
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * 
+ * Example 2:
+ * 
+ * Input: root = []
+ * Output: []
+ * 
+ * Example 3:
+ * 
+ * Input: root = [1]
+ * Output: [1]
+ */
 
 // Definition for a binary tree node.
 class TreeNode {
