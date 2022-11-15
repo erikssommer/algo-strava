88fd3084f54af0dbc39a93f43d5c7b099f26de9f
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-15 20:46:25 +0100

    Formatting of java code

--- java/easy/BinaryTreeInorder.java
+++ java/easy/BinaryTreeInorder.java
@@ -24,27 +24,33 @@ import java.util.List;
 
 // Definition for a binary tree node.
 class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
- }
- 
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class BinaryTreeInorder {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> inorder = new LinkedList<>();
         recInorder(root, inorder);
         return inorder;
     }
-    
-    private void recInorder(TreeNode node, List<Integer> inorder){
-        if (node != null){
+
+    private void recInorder(TreeNode node, List<Integer> inorder) {
+        if (node != null) {
             recInorder(node.left, inorder);
             inorder.add(node.val);
             recInorder(node.right, inorder);
