e731bbc26950d00bad591b451d892983b2a7e0e5
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-22 11:32:37 +0100

    Binary search to find target position

---
+++ python/easy/search_list_insert.py
@@ -0,0 +1,25 @@
+from ast import List
+import math
+
+
+class Solution:
+    def seachInsert(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums)-1
+            
+        if target > nums[len(nums)-1]:
+            return len(nums)
+            
+        if target < nums[start]:
+            return start
+            
+        while start <= end:
+            mid = math.floor((start + end) / 2)
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+            
+        return start
