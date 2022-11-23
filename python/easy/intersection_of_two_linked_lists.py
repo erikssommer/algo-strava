7041a83419b2b593b79bd9d4c35261fedb3dfcb6
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-23 14:47:12 +0100

    Finding where two linked lists meet

---
+++ python/easy/intersection_of_two_linked_lists.py
@@ -0,0 +1,28 @@
+""" 
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+"""
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        a, b = headA, headB
+
+        while a != b:
+            a = headB if not a else a.next
+            b = headA if not b else b.next
+
+        return a
